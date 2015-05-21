09bb5df8f0794635536a4286675f482de6d52fe6
LuoXiaohei <cn_lshang@qq.com>
2015-05-22 03:00:06 +0800

    update source files

--- deploy.bat
+++ deploy.bat
@@ -7,3 +7,7 @@ echo Start deploy
 cmd /c hexo d
 echo Clean files
 cmd /c hexo clean
+echo Start upload source files
+git add .
+git commit -a -m "update source files"
+git push origin master:master
