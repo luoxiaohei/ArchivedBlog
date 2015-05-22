c07f550755bca081d42a1fcb00499d9f07a32238
LuoXiaohei <cn_lshang@qq.com>
2015-05-23 03:09:20 +0800

    update source files

--- deploy.bat
+++ deploy.bat
@@ -9,7 +9,7 @@ echo Clean files
 cmd /c hexo clean
 echo Start upload source files
 git add .
-if %1 == "" (
+if "%1" == "" (
     git commit -a -m "update source files") else (
     git commit -a -m %1)
 git push origin master:source
