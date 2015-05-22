d62d8904f9a4d04d33efca0277e5bcb09ed83744
LuoXiaohei <cn_lshang@qq.com>
2015-05-23 03:45:22 +0800

    update source files

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,33 @@
 @echo off
+set auto=0
+set commit_message=%2
+
+if "%1" == "c" (
+    goto clean) else if "%1" == "d" (
+    goto deploy) else if "%1" == "u" (
+    goto upload) else (
+        set auto=1
+        set commit_message=%1
+    )
+
+:clean
 echo Start clean
 cmd /c hexo clean
-echo Start generate
-cmd /c hexo g
+if %auto% == 0 (
+    goto end)
+
+:deploy
 echo Start deploy
 cmd /c hexo d
-echo Clean files
-cmd /c hexo clean
+if %auto% == 0 (
+    goto end)
+
+:upload
 echo Start upload source files
 git add .
-if "%1" == "" (
-    git commit -m "update source files") else (
-    git commit -m %1)
+if "%commit_message%" == "" (
+    set commit_message="update source files")
+git commit -m %commit_message%
 git push origin master:source
+
+:end
